f985e9d61c3a2888f77c5135faf2fdfa9ca2703f
Olivia-Codes <fostero@mail.gvsu.edu>
2025-11-01 20:49:01 -0400

    Refactor Beverage component props and template structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
   <div>
-    <Beverage :isIced="beverageStore.currentTemp === 'Cold'" />
+    <Beverage 
+      :isIced="currentTemp === 'Cold'" 
+      :drink="currentBeverage" 
+      :creamer="currentCreamer"
+      :syrup="currentSyrup"
+    />
     <ul>
       <li>
-        <template v-for="temp in beverageStore.temps" :key="temp">
+        <h3>Temperature</h3>
+        <template v-for="temp in temps" :key="temp">
           <label>
             <input
               type="radio"
               name="temperature"
               :id="`r${temp}`"
               :value="temp"
-              v-model="beverageStore.currentTemp"
+              v-model="currentTemp"
             />
             {{ temp }}
           </label>
         </template>
       </li>
+      
+      <li>
+        <h3>Drink</h3>
+        <template v-for="beverage in bases" :key="beverage.id">
+          <label>
+            <input
+              type="radio"
+              name="drink"
+              :id="beverage.id"
+              :value="beverage.name"
+              v-model="currentBeverage"
+            />
+            {{ beverage.name }}
+          </label>
+        </template>
+      </li>
+      
+      <li>
+        <h3>Creamer</h3>
+        <template v-for="creamer in creamers" :key="creamer.id">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="creamer.id"
+              :value="creamer.name"
+              v-model="currentCreamer"
+            />
+            {{ creamer.name }}
+          </label>
+        </template>
+      </li>
+      
+      <li>
+        <h3>Syrup</h3>
+        <template v-for="syrup in syrups" :key="syrup.id">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="syrup.id"
+              :value="syrup.name"
+              v-model="currentSyrup"
+            />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
     </ul>
-    <input type="text" placeholder="Beverage Name" />
-    <button>🍺 Make Beverage</button>
   </div>
-  <div id="beverage-container" style="margin-top: 20px"></div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Beverage from "./components/Beverage.vue";
-import { useBeverageStore } from "./stores/beverageStore";
-const beverageStore = useBeverageStore();
+import { temps, bases, creamers, syrups, currentTemp } from "./stores/beverage";
+
+const currentBeverage = ref('Coffee');
+const currentCreamer = ref('No Cream');
+const currentSyrup = ref('No Syrup');
 </script>
 
 <style lang="scss">
@@ -40,7 +95,42 @@ html {
   background-color: #6e4228;
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
+
 ul {
   list-style: none;
+  padding: 20px;
+  
+  li {
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    
+    h3 {
+      color: #fff;
+      margin-right: 15px;
+      margin-bottom: 0;
+      font-size: 16px;
+      min-width: 120px;
+    }
+    
+    label {
+      display: inline-flex;
+      align-items: center;
+      color: #fff;
+      margin-right: 15px;
+      margin-bottom: 0;
+      cursor: pointer;
+      
+      input[type="radio"] {
+        margin-right: 5px;
+        cursor: pointer;
+      }
+      
+      &:hover {
+        color: #ffd89b;
+      }
+    }
+  }
 }
 </style>
